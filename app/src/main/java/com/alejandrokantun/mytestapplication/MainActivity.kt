748d7057d28acc1f2c491d5ff7b3654722f793c7
Alejandro Kantun <alejandrokantuncih@gmail.com>
2022-10-26 18:57:18 -0500

    Second version, changing image was added when button is clicked

--- app/src/main/java/com/alejandrokantun/mytestapplication/MainActivity.kt
+++ app/src/main/java/com/alejandrokantun/mytestapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.alejandrokantun.mytestapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import com.alejandrokantun.mytestapplication.databinding.ActivityMainBinding
 
@@ -10,6 +11,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val drawableResource = when (RollingDice()){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else ->R.drawable.dice_6
+        }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -18,11 +28,24 @@ class MainActivity : AppCompatActivity() {
     }
     fun RollDice(){
         binding.btnHola.setOnClickListener {
-        binding.tvResult.text = "Result of dice rolled: "+ RollingDice().toString()
+            val diceValue = RollingDice()
+            binding.tvResult.text = "Result of dice rolled: $diceValue"
+
+            val drawableResource = when (diceValue){
+                1->R.drawable.dice_1
+                2->R.drawable.dice_2
+                3->R.drawable.dice_3
+                4->R.drawable.dice_4
+                5->R.drawable.dice_5
+                else ->R.drawable.dice_6
+            }
+            binding.ivDiceImage.setImageResource(drawableResource)
         }
     }
 
     fun RollingDice():Int{
         return  (1..6).random()
     }
+
+
 }
